75561b67a94e1271a41e2a42a74ebfdb3416a876
Andrew Papacostas <a_papacostas@yahoo.com>
2016-07-29 04:46:02 +0000

    added h1 in layout in order_admin_html and h2 in order.html

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,12 +1,12 @@
 class StoreAdminController < ApplicationController
 
-  layout "admin"
-  def home
+  layout 'admin'
 
+  def home
   end
 
   def orders
-    render :layout =>  "order_administration"
+    render :layout => "order_administration"
   end
 
   def invoice
