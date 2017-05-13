3f5114806205d64a987afd5918fa2158875d345a
cquinones100 <cquinones100@gmail.com>
2017-05-13 19:52:15 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :store_admin_controllers
+  resources :static_controllers
   get 'home', to: 'static#home'
   get 'admin/home', to: 'store_admin#home'
   get 'admin/orders', to: 'store_admin#orders'
