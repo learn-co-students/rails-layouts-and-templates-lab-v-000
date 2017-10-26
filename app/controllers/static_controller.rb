2b6f5323f6973eece42786b6af303aae14bb0301
charlie lee <ctlee9096@gmail.com>
2017-10-26 03:33:31 +0000

    Done.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,5 @@
 class StaticController < ApplicationController
   def home
-    #render :layout => "home"
+    render :layout => "application"#customization route, for static.html.erb "default" exists
   end
 end
