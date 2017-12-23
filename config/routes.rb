ae3d2fb0f4268549d0e0035649f3a959baa3bafa
Oski <github email address>
2017-12-23 07:38:55 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
   get 'admin/home', to: 'store_admin#home'
   get 'admin/orders', to: 'store_admin#orders'
   get 'admin/invoice', to: 'store_admin#invoice'
+
+  get '/home', to: 'static#home'
+
+  get '/admin', to: 'store_admin#home'
+
+  get '/orders', to: 'store_admin#orders'
+
+  get '/invoice', to: 'store_admin#invoice'
+  
 end
