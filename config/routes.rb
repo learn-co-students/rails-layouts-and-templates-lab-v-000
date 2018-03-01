d383c41fdfe1b1cd63f4c64e218672e05cb528e2
Alex <github email address>
2018-03-01 08:03:41 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'home', to: 'static#home'
+        #interacts with static controller, home action, views/layouts/application.html.erb , views/static/home.html.erb
   get 'admin/home', to: 'store_admin#home'
+        #interacts with store_admin_controller, home action, views/layouts/admin.htm.erbl , views/store_admin/home.html.erb
   get 'admin/orders', to: 'store_admin#orders'
+        #interacts with store_admin_controller, orders action, views/layouts/order_administration.html.erb , views/store_admin/orders.html.erb
   get 'admin/invoice', to: 'store_admin#invoice'
+        #interacts with store_admin_controller, invoice action,  views/store_admin/orders.html.erb
 end
