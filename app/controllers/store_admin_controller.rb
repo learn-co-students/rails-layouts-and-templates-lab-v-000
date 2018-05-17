539cd8d05969073c61d6ef7e7e99b85592156096
Joanne Lee <vernapii@gmail.com>
2018-05-16 21:24:52 -0700

    Done.

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,8 +1,19 @@
 class StoreAdminController
   def admin
   end
-  
+
   def home
-    render :admin => "static"
+    render :admin
+  end
+
+  def orders
+    render :order_administration
+  end
+
+  def order_administration
+  end
+
+  def invoice
+    render :layout => false
   end
 end
