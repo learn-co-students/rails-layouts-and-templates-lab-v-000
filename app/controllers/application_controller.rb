e98bbb9e7da89a16cac1698d9b3998a49609e367
github username <github email address>
2018-05-19 06:23:42 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,3 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
+
+# How do I create the static controller route? Do I already have one?
+# I do have one but I need to make my StaticController
---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,9 @@
+class StaticController < ApplicationController
+  # I need to make a home view and a home method
+  # Or should I make a default layout view - application.html.erb?
+
+  def home
+    #how do I make this home view show? I'm missing the template.
+    #I had the wrong name for the home view
+  end
+end
