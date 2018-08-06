d1a4ce4da3aef00767067dc57660fa6ced3efbfd
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-08-06 17:56:49 +0000

    Done.

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -5,7 +5,7 @@ class StoreAdminController < ApplicationController
     end
 
     def orders
-        render :layout => "store_administration"
+        render :layout => "order_administration"
     end
 
     def invoice
