808c7e3411a685f15090c848936020ab595e0c5a
Anu Khambete <anukhambete@gmail.com>
2018-10-16 17:37:59 +0000

    Done.

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,7 +1,17 @@
 class StoreAdminController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
   def home
-    render :layout => "static"
+    render :layout => "admin"
   end
+
+  def orders
+    render :layout => "order_administration"
+  end
+
+  def invoice
+    render :layout => false
+  end
+
 end
