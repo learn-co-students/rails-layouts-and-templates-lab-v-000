10fa29fbb5d7a4777434fb278bcb26a3275990d2
github username <github email address>
2018-12-29 17:13:06 -0600

    Done.

---
+++ app/controllers/store_admin_controller.rb
@@ -0,0 +1,15 @@
+class StoreAdminController < ActionController::Base
+  layout "admin"
+
+  def home
+    #render :layout => "admin"
+  end
+
+  def orders
+    render :layout => "order_administration"
+  end
+
+  def invoice
+    render :layout => false
+  end
+end
