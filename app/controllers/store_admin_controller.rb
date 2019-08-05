ecb52f0e32306f3f5ac29e5af9b9958d8f40d1d2
NaKia Whitby <whitby.nakia@gmail.com>
2019-08-05 23:51:41 +0000

    Done.

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,15 +1,14 @@
 class StoreAdminController < ApplicationController
   layout "admin"
 
-  def invoice
-    render :layout => false
+  def home
   end
 
-   def orders
+  def orders
     render :layout => "order_administration"
   end
 
-  def home
-  end
-  
+  def invoice
+    render :layout => false
+  end  
 end
