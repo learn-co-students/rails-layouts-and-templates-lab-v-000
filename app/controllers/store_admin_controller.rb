55dd11f0571d1b8412925b4510073816c48bbc9e
Paul Fell <swellpf@gmail.com>
2019-08-12 07:22:01 -0400

    Done.

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,10 +1,14 @@
 class StoreAdminController < ApplicationController
-    def admin
+    layout "admin"
+    
+    def home
     end
-
+    
     def orders
+        render :layout => "order_administration"
     end
 
     def invoice
+        render :layout => false
     end
 end
